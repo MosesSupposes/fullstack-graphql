de5d673bac405be84221fb845c7019c61e5e6ec5
Moses Samuel <mosamuel98@gmail.com>
2020-02-12 11:38:22 -0800

    complete the "arguments and input types" exercise

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -4,31 +4,33 @@
  */
 
 module.exports = {
-  Query: {
-    // The first argument to a resolver is called the "initial value".
-    // It's the value (resolver) that came before it.
-    // The secnond argument is called "argument" -- this allows users to send arguments to the query.
-    // The third argument is the context object. This is the shared context amongst all of your resolvers.
-    // You can define the context in the apollo server configuration.
-    // The fourth argument is the AST of the incoming query. (This is an advanced technique for optimizing 
-    // your db queries, and is not commonly used.)
-    pets(_, filter, { models }) {
-      return filter ? 
-        models.Pet.findMany(filter) :
-        models.Pet.findMany()
-    }
-  },
-  // Mutation: {
-    
-  // },
-  // Pet: {
-  //   img(pet) {
-  //     return pet.type === 'DOG'
-  //       ? 'https://placedog.net/300/300'
-  //       : 'http://placekitten.com/300/300'
-  //   }
-  // },
-  // User: {
-    
-  // }
-}
+	Query: {
+		// The first argument to a resolver is called the "initial value".
+		// It's the value (resolver) that came before it.
+		// The secnond argument is an object called "argument" -- this allows users to send arguments to the query.
+		// The third argument is the context object. This is the shared context amongst all of your resolvers.
+		// You can define the context in the apollo server configuration.
+		// The fourth argument is the AST of the incoming query. (This is an advanced technique for optimizing
+		// your db queries, and is not commonly used.)
+		pets(_, { input }, { models }) {
+			return input ? models.Pet.findMany(input) : models.Pet.findMany();
+		},
+
+		pet(_, { input }, { models }) {
+			return input ? models.Pet.findOne(input) : models.Pet.findOne();
+		}
+	}
+	// Mutation: {
+
+	// },
+	// Pet: {
+	//   img(pet) {
+	//     return pet.type === 'DOG'
+	//       ? 'https://placedog.net/300/300'
+	//       : 'http://placekitten.com/300/300'
+	//   }
+	// },
+	// User: {
+
+	// }
+};
