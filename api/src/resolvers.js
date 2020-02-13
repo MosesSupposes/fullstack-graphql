255641f3aefc5fefe9ed08c510b74a8a8e8632d5
Moses Samuel <mosamuel98@gmail.com>
2020-02-13 13:21:52 -0800

    add clarifying comments

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -6,7 +6,9 @@
 module.exports = {
 	Query: {
 		// The first argument to a resolver is called the "initial value".
-		// It's the value (resolver) that came before it.
+		// It's the value of the resolver that came before it. Queries and mutations are top level resolvers.
+		// These always come first. The first argument is mainly used by field-level resolvers. Again, the first
+		// argument is the resolved value of the top level resolvers.
 		// The secnond argument is an object called "argument" -- this allows users to send arguments to the query.
 		// The third argument is the context object. This is the shared context amongst all of your resolvers.
 		// You can define the context in the apollo server configuration.
@@ -27,7 +29,10 @@ module.exports = {
 		}
 	},
 	Pet: {
-		owner(_, __, { models }) {
+		// The first argument is the Pet object. That is because in order for this resolver to run, the query
+		// for pet or pets had to have run first. The return type of those queries is of type Pet, so the first argument
+		// references the resolved value of that pet (all the fields that are stored for it in the database).
+		owner(pet, __, { models }) {
 			// There's only one user in the database.
 			const user = models.User.findOne();
 			return user;
